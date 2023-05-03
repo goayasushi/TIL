e8a0aae4e554dfe49dea49224cc17695b0d82236
goayasushi <goayasushi@gmail.com>
2023-05-03 09:19:13 +0900

    20230503

--- paiza/c68.rb
+++ paiza/c68.rb
@@ -1,31 +1,15 @@
 num = gets.to_i
-odd = [*"A".."Z"].rotate(-num)
-even = [*"A".."Z"].rotate(num)
-ciphers = gets.to_s.chars
-n = 0
+odd = [*"A".."Z"].rotate(-num).join
+even = [*"A".."Z"].rotate(num).join
+input = gets.to_s
+
 str = ""
 
-ciphers.each do |cipher|
-  if n % 2 != 0
-    str << cipher.tr("A-Z", odd)
-  else
-    str <<  cipher.tr("A-Z", even)
-  end
-end
-
-puts str
-
-
-
-cipher.each_char.with_index{|alpa, index|
+input.each_char.with_index(1){|alpa, index|
   if index % 2 != 0
-    alpa_new = alpa.tr("A-Z", odd)
-    puts alpa_new
-    str.push(alpa_new) 
+    str << alpa.tr("A-Z", odd)
   else
-    puts alpa
-    alpa_new =  alpa.tr("A-Z", even)
-    str.push(alpa_new) 
+    str << alpa.tr("A-Z", even)
   end
 }
 

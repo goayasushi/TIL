ab83d7b2a3acc0a629348e2f344c667742f2218a
goayasushi <goayasushi@gmail.com>
2023-05-11 09:32:34 +0900

    20230511

--- paiza/c69.rb
+++ paiza/c69.rb
@@ -1,3 +1,13 @@
+def elapsed_day(month,day)
+  elapsed_months = month - 1
+  if elapsed_months % 2 == 0
+    current_month_days = elapsed_months / 2 * 13 + elapsed_months / 2 * 15
+  else
+    current_month_days = (elapsed_months / 2 + 1) * 13 + elapsed_months / 2 * 15
+  end
+  return current_month_days + day
+end
+
 input = gets.split(' ').map(&:to_i)
 current_year = input[0]
 current_month = input[1]
@@ -15,29 +25,14 @@ until festival_year % 4 == 1 do
   festival_year += 1 
 end
 
-
-
-# 現在年の日数算出
-if (current_month - 1) % 2 == 0
-  current_month_days = (current_month - 1)/2*13 + (current_month - 1)/2*15
-else
-  current_month_days = ((current_month - 1)/2 + 1)*13 + (current_month - 1)/2*15
-end
-current_year_days = current_month_days + current_day
+# 現在年の経過日数算出
+current_year_days = elapsed_day(current_month,current_day)
 
 # 現在から開催までの年数
-years = 0
-until festival_year - current_year == 1 do
-  years += 1
-end
+years = festival_year - current_year - 1
 
 # 開催年の日数算出
-if (festival_month - 1) % 2 == 0
-  festival_month_days = festival_month/2*13 + festival_month/2*15
-else
-  festival_month_days = (festival_month/2 + 1)*13 + festival_month/2*15
-end
-festival_year_days = festival_month_days + festival_day
+festival_year_days = elapsed_day(festival_month,festival_day)
 
 days = (year_days - current_year_days) + years * year_days + festival_year_days
 puts days
